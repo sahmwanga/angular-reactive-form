a7a1a8c1faa809b6c038aa4699f9d80d5f662f9a
CodeSandbox <support@codesandbox.io>
2023-09-11 18:54:55 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CodeSandbox';
+
+  profileForm = new FormGroup({
+    firstName: new FormControl(""),
+    email: new FormControl("")
+  }) 
+
+  updateName(){
+  }
+
+  onSubmit(){
+    console.log(this.profileForm.value)
+  }
 }
